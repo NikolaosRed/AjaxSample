3532ac33651fdcfaca3298ed932df265c421393e
NickolaosRed <nickolasred@gmail.com>
2021-03-03 13:34:57 +0100

    Uploading project

---
+++ AjaxSample/WebAPI/Models/ProductDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.EntityClasses;
+
+namespace WebAPI.Models
+{
+    public partial class ProductDbContext : DbContext
+    {
+        public ProductDbContext(DbContextOptions<ProductDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
